61a56019103e4f3e7b857a16ef357e3cff53325d
Jun(Mike) Yao <yaojunmike@juns-MacBook-Pro-2.local>
2015-03-06 16:58:25 -0800

    stringifyJSON test cases passed.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,44 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (typeof(obj) === "number" || typeof(obj) == "boolean"){
+    return obj.toString();
+  }
+  else if (typeof(obj) === "string"){
+    return '"' + obj + '"';
+  }
+  else if (obj === null){
+    return "null";
+  }
+  else if (Array.isArray(obj)){
+    var result = '';
+    if (obj.length >= 1){
+      result = stringifyJSON(obj[0]);
+    }
+    for (var i = 1; i < obj.length; i++){
+      result = result + ',' + stringifyJSON(obj[i]);
+    }
+    return '[' + result + ']';
+  }
+  else if (typeof(obj) === "object"){
+    var result = '';
+
+    var isFirstTime = true;
+    for (var key in obj){
+      if (obj.hasOwnProperty(key)){
+        if (typeof(obj[key]) === "function" || obj[key] === undefined){
+          continue
+        }
+        if (isFirstTime){
+          result = result + "\"" + key + "\"" + ":" + stringifyJSON(obj[key]); 
+          isFirstTime = false;
+        }
+        else {
+          result = result + ',' + "\"" + key + "\"" + ":" + stringifyJSON(obj[key]); 
+        }
+      }
+    }
+    return '{' + result + '}';
+  }
 };
